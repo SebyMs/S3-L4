ab127e6e2fac22227bcf13cd40fe324f5eebe8f0
SebyMs <sebastiano.monello02@gmail.com>
2023-07-13 23:50:01 +0200

    Index

---
+++ Untitled-1.js
@@ -0,0 +1,63 @@
+const creaTabellone = function () {
+  const board = document.querySelector(".board-tabelle");
+  for (let i = 0; i < 76; i++) {
+    board.innerHTML += `<div class='cell' '>${i + 1}</div>`;
+  }
+};
+creaTabellone();
+// id='${i + 1}'
+const creaTabelline = function () {
+  const input = document.querySelector("input");
+  const container = document.querySelector("main");
+  let inputValue = parseInt(input.value);
+  let y = 0;
+  let z = 0;
+  for (let i = 0; i < inputValue; i++) {
+    const tabelle = document.createElement("div");
+    tabelle.innerHTML = "";
+    // tabelle.setAttribute("id", i);
+    tabelle.setAttribute("class", "tabelline-grandi");
+    tabelle.setAttribute("id", i);
+    container.appendChild(tabelle);
+    let divTabelle = document.getElementById(i);
+    console.log(divTabelle);
+    z += 1;
+    x = 0;
+    while (x !== 15) {
+      const randNum = Math.floor(Math.random() * 100);
+      const tabelline = document.createElement("div");
+      tabelline.innerHTML = ``;
+      tabelline.setAttribute("class", "tabelline");
+
+      if (randNum <= 76 && randNum !== 0) {
+        tabelline.innerHTML = `${randNum}`;
+        divTabelle.appendChild(tabelline);
+        x += 1;
+        y += 1;
+      }
+    }
+  }
+};
+
+array = [0];
+const randNum = function () {
+  let x = 0;
+  while (x !== 1) {
+    const randNumber = Math.floor(Math.random() * 100);
+    if (randNumber <= 76 && randNumber !== 0) {
+      array.push(randNumber);
+      x = 1;
+    }
+  }
+  let casella = 0;
+  const div = document.querySelectorAll(".board-tabelle div");
+  for (let i = 0; i < div.length; i++) {
+    if (parseInt(div[i].innerHTML) === array[array.length - 1]) {
+      casella = array[array.length - 1];
+      div[i].style.backgroundColor = "red";
+    }
+  }
+};
+// cambiare sistema di assegnazione a tabelle grandi
+// dare id al
+// aggiungere selezione tabbelline
